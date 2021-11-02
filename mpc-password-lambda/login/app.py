4634d317b57929bc1c4fedb5b2d4fcf0c15842e9
0riv3r <ofer.rivlin@gmail.com>
2021-11-02 16:57:48 +0200

    added async and wait

--- mpc-password-lambda/login/app.py
+++ mpc-password-lambda/login/app.py
@@ -11,9 +11,11 @@ from mpyc.runtime import mpc
 
 nest_asyncio.apply() # https://pypi.org/project/nest-asyncio/
 
+stored_password = "e&t.C_^q2RFrP%"
+
 secint = mpc.SecInt()
 
-def encode_to_int(text):
+async def encode_to_int(text):
     # print(f'Encoding {text}')
     text_bytes = text.encode('utf-8') + b'\x01'  # Pad with 1 to preserve trailing zeroes
     text_int = int.from_bytes(text_bytes, 'little')
@@ -23,39 +25,10 @@ def encode_to_int(text):
     # recoveredstring = recoveredbytes[:-1].decode('utf-8') # Strip pad before decoding
     # print(recoveredstring)
 
-
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
+async def main(password):
     res = ''
-    stored_password = "e&t.C_^q2RFrP%"
-    password = ''
-    try:
-        password = event['password']
-    except KeyError:
-        password = event['queryStringParameters']['password']
-
-    encoded_stored_password = encode_to_int(stored_password)
-    encoded_password = encode_to_int(password)
+    encoded_stored_password = await encode_to_int(stored_password)
+    encoded_password = await encode_to_int(password)
 
     # secret integers
     encrypted_stored_password = secint(encoded_stored_password)
@@ -68,7 +41,28 @@ def lambda_handler(event, context):
         res = "Login succeed"
     else:
         res = "Passwords don't match"
+        
+    return res
 
+
+def lambda_handler(event, context):
+    """Login Lambda function
+
+    Parameters
+    ----------
+    event: password (string), required
+
+    Returns
+    ------
+    String: correct or wrong password
+    """
+    
+    try:
+        password = event['password']
+    except KeyError:
+        password = event['queryStringParameters']['password']
+
+    res = asyncio.run(main(password))
 
     return {
         "statusCode": 200,
